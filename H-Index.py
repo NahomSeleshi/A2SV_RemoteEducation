1f48d498fb05b169500e5b44d627c7154ac5daf7
NahomSeleshi <nahomseleshi43@gmail.com>
2022-08-31 22:25:13 -0400

    H-Index.py is added to the repo A2SV_RemoteEducation

---
+++ H-Index.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        
+        citations.sort(reverse=True)
+        
+        for i,c in enumerate(citations):
+            if i >= c:
+                return i
+            
+        return len(citations)

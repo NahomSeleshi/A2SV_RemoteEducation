4322fb681c0c2c6d545d5b16c7b97593df4d65d7
NahomSeleshi <nahomseleshi43@gmail.com>
2023-01-11 23:02:20 -0500

    Find_first_and_last_position_of_element_in_a_sorted_array.py is added to the repo A2SV_RemoteEducation

--- find_first_and_last_position_of_element_in_a_sorted_array.py
+++ find_first_and_last_position_of_element_in_a_sorted_array.py
@@ -1,31 +1,34 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        answer = [-1,-1]
+        self.answer = [-1, -1]
         if len(nums) == 0:
-            return answer
-        def binarySearchLeft(element):
-            left, right = 0, len(nums)-1
-            while left <= right:
-                mid = left + (right - left)//2
-                if nums[mid] == target:
-                    answer[0] = mid
-                    right = mid - 1
-                elif nums[mid] > target:
-                    right = mid - 1
-                else:
-                    left = mid + 1
-    
-        def binarySearchRight(element):
-            left, right = 0, len(nums)-1
-            while left <= right:
-                mid = left + (right - left)//2
-                if nums[mid] == target:
-                    answer[1] = mid
-                    left = mid + 1
-                elif nums[mid] > target:
-                    right = mid - 1
-                else:
-                    left = mid + 1
-        binarySearchLeft(target)
-        binarySearchRight(target)
-        return answer
+            return self.answer
+
+        self.binary_search_left(nums, target)
+        self.binary_search_right(nums, target)
+
+        return self.answer
+
+    def binary_search_left(self, array, target):
+        left, right = 0, len(array) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if array[mid] == target:
+                self.answer[0] = mid
+                right = mid - 1
+            elif array[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+    def binary_search_right(self, array, target):
+        left, right = 0, len(array) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if array[mid] == target:
+                self.answer[1] = mid
+                left = mid + 1
+            elif array[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1

97232739ab0d5da62d811f36ea7d9c0a29ad0ba8
NahomSeleshi <81203642+NahomSeleshi@users.noreply.github.com>
2023-01-14 21:19:24 -0500

    Update Capacity_to_ship_packages_with_D_days.py

--- Capacity_to_ship_packages_with_D_days.py
+++ Capacity_to_ship_packages_with_D_days.py
@@ -1,19 +1,13 @@
 class Solution:
 
     def days_spent(self, capacity, weights):
-
         cur_weight = 0
-
         cur_days = 1
 
         for i in range(len(weights)):
-
             cur_weight += weights[i]
-
             if cur_weight > capacity:
-
                 cur_weight = weights[i]
-
                 cur_days += 1
 
         return cur_days
@@ -21,29 +15,18 @@ class Solution:
 
 
     def shipWithinDays(self, weights: List[int], days: int) -> int:
-
         left, right = max(weights), sum(weights)
-
         least_weight = sum(weights)
 
-        
-
         while left <= right:
-
             mid = left + (right - left)//2
-
             cur_days = self.days_spent(mid, weights)
 
             if cur_days > days:
-
                 left = mid + 1
 
             else:
-
                 right = mid - 1
-
                 least_weight = min(least_weight, mid)
-
-
 
         return least_weight
